757d357b176ff4ee634e0b1a6b909bdc5c8b9ce2
Amit Kumar Soni <amitsoniforbhu@gmail.com>
2022-08-09 13:30:59 +0530

    express app added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const PORT = process.env.port || 3000;
+app.use(express.json());
+
+// routes starts here
+app.post("/slack/events", (req, res) => {
+  res.send({
+    challange: req.body.challange,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Magic happens at port", PORT);
+});
